22fd6158357d6f2ff44b0b7125e4cc3523edff0b
Adrian <itsadriandayrit@gmail.com>
2020-06-14 12:04:14 +0800

    Update FfmpegCapturer.kt

--- src/main/kotlin/org/jitsi/jibri/capture/ffmpeg/FfmpegCapturer.kt
+++ src/main/kotlin/org/jitsi/jibri/capture/ffmpeg/FfmpegCapturer.kt
@@ -40,9 +40,9 @@ import java.util.logging.Logger
 data class FfmpegExecutorParams(
     val resolution: String = "1280x720",
     val framerate: Int = 30,
-    val videoEncodePreset: String = "fast",
+    val videoEncodePreset: String = "faster",
     val queueSize: Int = 4096,
-    val streamingMaxBitrate: Int = 1200,
+    val streamingMaxBitrate: Int = 2000,
     val streamingBufSize: Int = streamingMaxBitrate * 2,
         // The range of the CRF scale is 0–51, where 0 is lossless,
         // 23 is the default, and 51 is worst quality possible. A lower value
@@ -51,7 +51,7 @@ data class FfmpegExecutorParams(
         // it should look the same or nearly the same as the input but it
         // isn't technically lossless.
         // https://trac.ffmpeg.org/wiki/Encode/H.264#crf
-    val h264ConstantRateFactor: Int = 28,
+    val h264ConstantRateFactor: Int = 30,
     val gopSize: Int = framerate * 2
 )
 
